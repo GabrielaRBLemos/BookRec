55033de624bf0043a65e4a802855628371fcc582
Gabriela Lemos <124183874+GabrielaRBLemos@users.noreply.github.com>
2024-04-12 15:08:09 +0000

    added getters and setters

--- Source/livro.java
+++ Source/livro.java
@@ -1,3 +1,26 @@
 public class livro {
+    String book_id[];
+    String title[100];
+    int publicationYear;
+    ArrayList<String> descriptor;
+    public String[] getBook_id() {
+        return book_id;
+    }
+    public void setBook_id(String[] book_id) {
+        this.book_id = book_id;
+    }
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+    public ArrayList<String> getDescriptor() {
+        return descriptor;
+    }
+    public void setDescriptor(ArrayList<String> descriptor) {
+        this.descriptor = descriptor;
+    }
+    
     
 }
