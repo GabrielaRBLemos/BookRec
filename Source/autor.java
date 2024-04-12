f185e5a76db43715b4fe36612357fa8cb4cd6131
JuliaSilva05 <128699860+JuliaSilva05@users.noreply.github.com>
2024-04-12 12:34:28 -0300

    Update autor.java

--- Source/autor.java
+++ Source/autor.java
@@ -1,38 +1,46 @@
 public class autor {
+
     String nome;
-  String pais;
-  String nascimento;
-  String estado;
-  String id;
+    String pais;
+    String nascimento;
+    String estado;
+    String id;
+    public autor(String nome, String pais, String nascimento, String estado, String id){
+        this.nome = nome;
+        this.pais = pais;
+        this.nascimento = nascimento;
+        this.estado = estado;
+        this.id = id;
+    }
   
-  public String getNome() {
-  	return nome;
-  }
-  public void setNome(String nome) {
-  	this.nome = nome;
-  }
-  public String getPais() {
-  	return pais;
-  }
-  public void setPais(String pais) {
-  	this.pais = pais;
-  }
-  public String getNascimento() {
-  	return nascimento;
-  }
-  public void setNascimento(String nascimento) {
-  	this.nascimento = nascimento;
-  }
-  public String getEstado() {
-  	return estado;
-  }
-  public void setEstado(String estado) {
-  	this.estado = estado;
-  }
-  public String getId() {
-  	return id;
-  }
-  public void setId(String id) {
-  	this.id = id;
-  }
+    public String getNome() {
+      return nome;
+    }
+    public void setNome(String nome) {
+      this.nome = nome;
+    }
+    public String getPais() {
+      return pais;
+    }
+    public void setPais(String pais) {
+      this.pais = pais;
+    }
+    public String getNascimento() {
+      return nascimento;
+    }
+    public void setNascimento(String nascimento) {
+      this.nascimento = nascimento;
+    }
+    public String getEstado() {
+      return estado;
+    }
+    public void setEstado(String estado) {
+      this.estado = estado;
+    }
+    public String getId() {
+      return id;
+    }
+    public void setId(String id) {
+      this.id = id;
+    }
 }
