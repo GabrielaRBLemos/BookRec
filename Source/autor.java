bdf19bb1fb5ae461b4f630961a575d470bbb9713
Gabriela Lemos <124183874+GabrielaRBLemos@users.noreply.github.com>
2024-04-30 11:52:34 -0300

    Updated Autor
    changed the order of  variables in Autor for better reading comprehension

--- Source/autor.java
+++ Source/autor.java
@@ -1,18 +1,24 @@
 public class autor {
-
+    String id;
     String nome;
     String pais;
     String nascimento;
     String estado;
-    String id;
     public autor(String nome, String pais, String nascimento, String estado, String id){
-        this.nome = nome;
-        this.pais = pais;
-        this.nascimento = nascimento;
-        this.estado = estado;
-        this.id = id;
+      this.id = id;
+      this.nome = nome;
+      this.pais = pais;
+      this.nascimento = nascimento;
+      this.estado = estado;
+
     }
   
+    public String getId() {
+      return id;
+    }
+    public void setId(String id) {
+      this.id = id;
+    }
     public String getNome() {
       return nome;
     }
@@ -36,11 +42,5 @@ public class autor {
     }
     public void setEstado(String estado) {
       this.estado = estado;
-    }
-    public String getId() {
-      return id;
-    }
-    public void setId(String id) {
-      this.id = id;
     }
 }
