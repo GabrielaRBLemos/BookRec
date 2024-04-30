7c6afb3d17705aa4ad617b7d7d9059e3f3530413
JuliaSilva05 <128699860+JuliaSilva05@users.noreply.github.com>
2024-04-30 12:29:07 -0300

    Update BookRec.java

--- Source/BookRec.java
+++ Source/BookRec.java
@@ -1,5 +1,12 @@
-public class BookRec {
-  public static void main(String[] args) {
+import java.util.Scanner;  
 
-  }
+public class BookRec {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(new File("Data/Autores.csv"));
+        reader.useDelimiter(",");
+        while (reader.hasNext()){
+            System.out.println(reader.next());
+        }
+        reader.close();
+    }
 }
