db06d84a22f6feba3242b5596622152b84b546f7
Gabriela Lemos <124183874+GabrielaRBLemos@users.noreply.github.com>
2024-05-02 11:52:21 -0300

    Add files via upload

--- Source/BookRec.java
+++ Source/BookRec.java
@@ -1,32 +1,52 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class BookRec {
     
     public static String changeAuthorsPathBasedOnOS(){
         String osName = System.getProperty("os.name").toLowerCase();
-        if (osName.equals("windows")) {
+        if (osName.contains("windows")) {
             return "Data\\Autores.csv";             
         }
         return "Data/Autores.csv";
     }
 
-    public static void readAutores(String AuthorsFile){
-        AuthorsFile = changeAuthorsPathBasedOnOS();
+    public static List<autor> readAutores(){
+        String authorsFile = changeAuthorsPathBasedOnOS();
+        List<autor> authors = new ArrayList<>();
         
-        try (Scanner scanner = new Scanner(new File(AuthorsFile))) {
+        try (Scanner scanner = new Scanner(new File(authorsFile))) {
             scanner.useDelimiter(",|\\n");
             while (scanner.hasNext()) {
-                System.out.print(scanner.next() + "\t");
+                String id = scanner.next();
+                String nome = scanner.next();
+                String pais = scanner.next();
+                String nascimento = scanner.next();
+                boolean isAlive = Boolean.parseBoolean(scanner.next());
+                
+                autor newAutor = new autor(nome, pais, nascimento, isAlive, id);
+                authors.add(newAutor);
             }
         } catch (FileNotFoundException e) {
-            System.err.println("File not found: " + AuthorsFile);
+            System.err.println("File not found: " + authorsFile);
             e.printStackTrace();
         }
+        
+        return authors;
     }
 
     public static void main(String[] args) {
-        readAutores(changeAuthorsPathBasedOnOS());
+        List<autor> authors = readAutores();
+        for (autor a : authors) {
+            System.out.println("ID: " + a.getId());
+            System.out.println("Nome: " + a.getNome());
+            System.out.println("País: " + a.getPais());
+            System.out.println("Nascimento: " + a.getNascimento());
+            System.out.println("Is Alive: " + a.getIsAlive());
+            System.out.println();
+        }
     }
 }
--- Source/livroLido.java
+++ Source/livroLido.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class livroLido extends livro{
     private boolean possui;
+    private float rating;
 
 
     public livroLido(String[] idAutor, String[] title, int publicationYear, ArrayList<String> descriptor,
@@ -16,5 +17,18 @@ public class livroLido extends livro{
 
     public void setPossui(boolean possui) {
         this.possui = possui;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        if (rating >= 0 && rating <= 5) {
+            this.rating = rating;
+        }
+        else{
+            System.out.println("Rating can only be between 0 and 5.\n");
+        }
     }
 }
