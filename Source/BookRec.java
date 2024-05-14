9c720cd7f8506e8f628657609e6977aba7f3a898
JuliaSilva05 <128699860+JuliaSilva05@users.noreply.github.com>
2024-05-14 15:08:15 -0300

    Update BookRec.java
    traduzindo os nomes das variaveis

--- Source/BookRec.java
+++ Source/BookRec.java
@@ -8,7 +8,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class BookRec {
-    
+
     public static String changeAuthorsPathBasedOnOS(){
         String osName = System.getProperty("os.name").toLowerCase();
 
@@ -26,11 +26,11 @@ public class BookRec {
         return "Data/Read.csv";
     }
 
-    public static ArrayList<autor> readAuthors(){
+    public static ArrayList<author> readAuthors(){
 
         String authorsFile = changeAuthorsPathBasedOnOS();
-        ArrayList<autor> authors = new ArrayList<>();
-        
+        ArrayList<author> authors = new ArrayList<>();
+
         try (Scanner scanner = new Scanner(new File(authorsFile))) {
 
             scanner.useDelimiter(",|\\n");
@@ -38,13 +38,13 @@ public class BookRec {
             while (scanner.hasNext()) {
 
                 String id = scanner.next();
-                String nome = scanner.next();
-                String pais = scanner.next();
-                String nascimento = scanner.next();
+                String name = scanner.next();
+                String country = scanner.next();
+                String birthYear = scanner.next();
                 boolean isAlive = Boolean.parseBoolean(scanner.next());
-                
-                autor newAutor = new autor(nome, pais, nascimento, isAlive, id);
-                authors.add(newAutor);
+
+                author newAuthor = new author(name, country, birthYear, isAlive, id);
+                authors.add(newAuthor);
             }
         } catch (FileNotFoundException e) {
             System.err.println("File not found: " + authorsFile);
@@ -52,25 +52,25 @@ public class BookRec {
         }
         return authors;
     }
-    
-    public static ArrayList<livroLido> readReadBooks(){
-        String readBooksFile = changeReadPathBasedOnOS();
-        ArrayList<livroLido> readBookList = new ArrayList<>();
+
+    public static ArrayList<readBook> readReadBooks(){
+        String readBookListFile = changeReadPathBasedOnOS();
+        ArrayList<readBook> readBookList = new ArrayList<>();
     }
 
     /*WORK IN PROGRESS */
-    // public static ArrayList<livroLido> readReadBooks(){
-    //     String readBooksFile = changeReadPathBasedOnOS();
-    //     ArrayList<livroLido> readBookList = new ArrayList<>();
+    // public static ArrayList<readBook> readReadBooks(){
+    //     String readBookListFile = changeReadPathBasedOnOS();
+    //     ArrayList<readBook> readBookList = new ArrayList<>();
     //     String Line;
-    //         try (Scanner scanner = new Scanner(new File(readBooksFile))) {
+    //         try (Scanner scanner = new Scanner(new File(readBookListFile))) {
     //             while (scanner.hasNextLine()) {
     //                 line = scanner.nextLine();
     //                 String[] parts = line.split(",(?![^\\[]*\\])");
     //             }
     //         }
     //         catch (FileNotFoundException e){
-    //             System.err.println("File not found: " + readBooksFile);
+    //             System.err.println("File not found: " + readBookListFile);
     //             e.printStackTrace();
     //         }
     //     return readBookList;
@@ -78,18 +78,18 @@ public class BookRec {
     /*END OF WORK IN PROGRESS */
 
     public static void main(String[] args) {
-        ArrayList<autor> authors = readAuthors();
-        // ArrayList<livroLido> readBooks = readReadBooks();
-        for (autor a : authors) {
+        ArrayList<author> authors = readAuthors();
+        // ArrayList<readBook> readBookList = readReadBooks();
+        for (author a : authors) {
             System.out.println("ID: " + a.getId());
-            System.out.println("Nome: " + a.getNome());
-            System.out.println("País: " + a.getPais());
-            System.out.println("Nascimento: " + a.getNascimento());
+            System.out.println("Name: " + a.getName());
+            System.out.println("Country: " + a.getCountry());
+            System.out.println("Birth Year: " + a.getBirthYear());
             System.out.println("Is Alive: " + a.getIsAlive());
             System.out.println();
         }
-        for (livroLido r : readBooks) {
-            System.out.println("titulo: "+ r.getTitle());
+        for (readBook r : readBookList) {
+            System.out.println("Title: "+ r.getTitle());
         }
     }
 }
